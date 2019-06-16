b0ee2bf426c49afdc35f61fef798e3e4b4fa000f
Renée Cruz <reneebasscruz@gmail.com>
2019-06-16 21:32:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
-
+def my_collect(array)
+  i = 0
+  new_array = []
+  
+  while i < array.length
+  
+    yield array[i]
+    i += 1
+    new_array.push(array[i])
+   
+  end
+  
+  new_array
+end
 
